49344f79a1db43a9b22b238f5b727287e531efde
Alexey Zharov <to.alex.zharov@gmail.com>
2016-09-05 16:03:24 +0300

    first commit

---
+++ TryingAspCoreAndNancy/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace TryingAspCoreAndNancy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls(args)
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ TryingAspCoreAndNancy/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TryingAspCoreAndNancy")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88785f22-dc06-4360-a3da-0d00a1bae991")]
