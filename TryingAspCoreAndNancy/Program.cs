bee89fe566dcbbcb5be5b769f930ecab0d755499
Alexey Zharov <to.alex.zharov@gmail.com>
2016-09-05 16:27:30 +0300

    Added UseContentRoot

--- TryingAspCoreAndNancy/Program.cs
+++ TryingAspCoreAndNancy/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace TryingAspCoreAndNancy
 {
@@ -7,6 +8,7 @@ namespace TryingAspCoreAndNancy
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseKestrel()
                 .UseStartup<Startup>()
                 .UseUrls(args)
